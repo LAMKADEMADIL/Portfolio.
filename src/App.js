612ec91a6160d73ce210f868a972e3b13d6752c3
LAMKADEMADIL <adillamkadem215@gmail.com>
2026-07-03 00:07:18 +0100

    creacte sur longuge de page header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react"
 import Head from "./page/Head";
+import { Home } from "./page/Home";
+import { About } from "./page/About";
+import { Project } from "./page/Project";
+import { Contact } from "./page/Contact";
+import { BrowserRouter , Router , Routes , Route , Link } from "react-router-dom";
 
 function App () {
   return (
     <div>
-      <h1 align="center"> lamkadem adil </h1>
+      <BrowserRouter>
        <Head />
+       <Routes>
+         <Route path="/" element={<Home />} />
+         <Route path="/About" element={<About />} />
+         <Route path="/Project" element={<Project />} />
+         <Route path="/Contact" element={<Contact />} />
+       </Routes>
+       </BrowserRouter>
     </div>
   )
 }
--- src/page/Head.jsx
+++ src/page/Head.jsx
@@ -1,11 +1,22 @@
+import { Link } from "react-router-dom"
+
 export default function Head () {
     return(
-        <header>
+        <header> 
             <h1> Portfolio </h1>
             <nav>
                 <ul>
                     <li>
-                        Home
+                        <Link to="/" > Home </Link>
+                    </li>
+                    <li>
+                        <Link to="/About" > About </Link>
+                    </li>
+                    <li>
+                        <Link to="/Project" > Project </Link>
+                    </li>
+                    <li>
+                        <Link to="/Contact" > Contact </Link>
                     </li>
                 </ul>
             </nav>

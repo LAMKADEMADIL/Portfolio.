a782d510ae98e986d7a81996c3ad579707006ea3
LAMKADEMADIL <adillamkadem215@gmail.com>
2026-07-03 23:37:18 +0100

    creacte la page nabare 1 sur h1 avce image

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,9 +1,17 @@
 export function Home() {
   return (
     <div className="Home">
-      <section>
-        <h1> adil lamkadem </h1>
-        <h2> Front-End Developer </h2>
+      <div className="left">
+        <span className="hello"> Hello, I'm</span>
+
+        <h1>
+          adil <span>lamkadem</span>
+        </h1>
+
+        <h2>
+          Front-End <span>Developer</span>
+        </h2>
+
         <p>
           I am a passionate front-end developer with a strong focus on creating
           visually appealing and user-friendly web applications. With a solid
@@ -12,10 +20,17 @@ export function Home() {
           staying up-to-date with the latest industry trends to ensure that my
           work meets the highest standards of quality and innovation.
         </p>
-      </section>
-      <section>
-        <img src="/path/to/your/image.jpg" alt="adil lamkadem" />
-      </section>
+
+        <div className="buttons">
+          <button className="hire">Hire Me</button>
+
+          <button className="cv">Download CV</button>
+        </div>
+      </div>
+
+      <div className="right">
+        <img src="/ADIL.jpeg" alt="profile" />
+      </div>
     </div>
   );
 }
